a9e6abfbd13cdc6018e24d7ef1fc368fe9a054b5
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-09-30 17:31:40 -0300

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../components/Home.vue';
+import PokemonList from '../components/PokemonList.vue';
 
 Vue.use(VueRouter)
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
-
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/Pokedex',
+    name: 'PokemonList',
+    component: PokemonList
+  },
 ]
 
 const router = new VueRouter({
