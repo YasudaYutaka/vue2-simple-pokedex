7af4b0ae8cf17aa1df589cc169b407aa1a8dd6bc
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-09-30 17:31:34 -0300

    AppToolBar

---
+++ src/components/shared/AppToolBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="app-top-bar">
+    <v-app-bar color="deep-purple accent-4 indigo darken-3" app dark>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>Pokédex</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <router-link to="/">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
+          </router-link>  
+
+          <router-link to="/Pokedex">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Pokédex</v-list-item-title>
+            </v-list-item>
+          </router-link>    
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
+</script>
+
+<style scoped>
+  .app-top-bar a {
+    text-decoration: none;
+  }
+</style>
