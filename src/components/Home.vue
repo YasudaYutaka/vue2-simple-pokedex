4501d13c7d76fb327c692ac0aa4abff0636d146c
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-09-30 14:28:20 -0300

    Home.vue

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container class="mt-auto">
+    <v-img class="ma-auto" src="https://wallpaper.dog/large/20437316.jpg" max-width="1000" />
+    <p class="subheading text-center mt-5">
+      Pokémon (an abbreviation for Pocket Monsters in Japan) is a Japanese media
+      franchise managed by The Pokémon Company, a company founded by Nintendo,
+      Game Freak, and Creatures.
+    </p>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
