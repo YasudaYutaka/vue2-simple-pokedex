b4469eccd89e3cb2c1d267ea07d3c76a0bd6774a
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-10-01 01:36:33 -0300

    pokemon filter & galar typo

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,15 +1,23 @@
 <template>
   <v-container class="my-md-10 mt-xs-5">
 
-    <v-row class="mb-10" wrap row justify-space-around>
+    <v-row class="mb-3" wrap row justify-space-around>
       <v-col cols="4" sm="2" class="text-center" v-for="region in pokemonRegions" :key="region">
         <v-btn :class="selectedRegion === region ? 'active' : 'blue'" small dark @click="regionSort(region)">{{firstLetterUpperCase(region)}}</v-btn>
       </v-col>
     </v-row>
 
-    <h2 class="mb-10 text-center text-h3">{{firstLetterUpperCase(selectedRegion)}}</h2>
+    <v-row justify="center">
+      <input class="searchInput" v-model="searchedPokemon">
+      <v-btn @click="searchPokemons(searchedPokemon)" small>Search</v-btn>
+    </v-row> 
 
-    <v-card class="px-3">
+    <v-row justify="center" class="my-10">
+      <h2 class="text-center text-h3">{{firstLetterUpperCase(selectedRegion)}}</h2>
+    </v-row>
+
+
+    <v-card class="px-3" v-if="!isSearched">
       <v-row row wrap justify-center>
           <v-col cols="6" md="3" v-for="(pokemon, index) in pokemonList.results" :key="index">
             <v-img :src='`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1+pokemonNumber}.png`' class="ma-auto" max-width="150"/>
@@ -17,6 +25,16 @@
           </v-col>
       </v-row>
     </v-card>
+
+    <v-card class="px-3" v-else>
+      <v-row row wrap justify-center>
+          <v-col cols="6" md="3" v-for="(pokemon, index) in pokemonList.results" :key="index">
+            <v-img :src='`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getPokemonNumber(index)}.png`' class="ma-auto" max-width="150"/>
+            <div class="text-sm-caption text-lg-h5 text-center">{{firstLetterUpperCase(pokemon.name)}}</div>
+          </v-col>
+      </v-row>
+    </v-card>
+
 
   </v-container>
 </template>
@@ -28,7 +46,10 @@ export default {
       pokemonList: [],
       pokemonNumber: 0,
       pokemonRegions: ['kanto', 'johto', 'hoenn', 'sinnoh', 'unova', 'kalos', 'alola', 'galar'],
-      selectedRegion: ''
+      selectedRegion: '',
+      searchedPokemon: '',
+      isSearched: false,
+      pokemonImgUrl: []
     };
   },
   created() {
@@ -80,11 +101,12 @@ export default {
         case 'galar':
           this.fetchPokemon(89, 809);
           this.pokemonNumber = 809;
-          this.selectedRegion = 'galos';
+          this.selectedRegion = 'galar';
           break;      
       }
     },
     fetchPokemon(limit, offset) {
+      this.isSearched= false;
       fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
         .then((response) => response.json())
         .then((json) => {
@@ -93,17 +115,72 @@ export default {
     },
     firstLetterUpperCase(string) {
       return string.charAt(0).toUpperCase() + string.slice(1);
+    },
+    searchPokemons(sPokemon) {
+      this.regionSort(this.selectedRegion);
+      this.pokemonImgUrl = [];
+      this.pokemonList.results = this.pokemonList.results.filter((pokemon) => {
+        return pokemon.name.match(sPokemon.toLowerCase());
+      });
+      for(let data of this.pokemonList.results) {
+        this.pokemonImgUrl.push(data.url);
+      }
+      this.isSearched = true;
+      this.searchedPokemon = "";
+    },
+    getPokemonNumber(index) {
+        return this.pokemonImgUrl[index].match(/\d+/g)[1];
     }
   }
 };
 </script>
 
 <style scoped>
+  .searchInput {
+    border: 1px solid blue;
+  }
+
   .active {
     background-color: green;
   }
 
   .blue {
     background-color: blue;
+  }
+
+  .slide-enter {
+    opacity: 0;
+  }
+
+  .slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+    transition: opacity .5s;
+  }
+
+  .slide-leave {
+
+  }
+
+  .slide-leave-active {
+    transition: opacity .5s;
+    opacity: 0;
+  }
+
+  @keyframes slide-in {
+      from {
+          transform: translateY(20px);
+      }
+      to {
+          transform: translateY(0);
+      }
+  }
+
+  @keyframes slide-out {
+      from {
+        transform: translateY(0);
+      }
+      to {
+        transform: translateY(20px);
+      }
   }
 </style>
