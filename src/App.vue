b591af2e75dc7f264214072517233a924fbba3ea
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-09-30 23:59:04 -0300

    animation

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <AppToolBar />
     
     <v-main>
-     <router-view />
+      <transition name="slide" type="animation" appear>
+        <router-view />
+      </transition>
     </v-main>
   </v-app>
 </template>
@@ -18,6 +20,40 @@ import AppToolBar from './components/shared/AppToolBar.vue';
 </script>
 
 <style>
+  .slide-enter {
+    opacity: 0;
+  }
 
+  .slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+    transition: opacity .5s;
+  }
+
+  .slide-leave {
+
+  }
+
+  .slide-leave-active {
+    transition: opacity .5s;
+    opacity: 0;
+  }
+
+  @keyframes slide-in {
+      from {
+          transform: translateY(20px);
+      }
+      to {
+          transform: translateY(0);
+      }
+  }
+
+  @keyframes slide-out {
+      from {
+        transform: translateY(0);
+      }
+      to {
+        transform: translateY(20px);
+      }
+  }
 </style>
 
